6b4cbaabaccfd8db0c841c935251d8fdece539aa
mishoba s <mishose88@gmail.com>
2020-06-16 15:44:32 +0530

    test push

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-
+/*test*/
 class User extends Authenticatable
 {
     use Notifiable,HasApiTokens,HasRoles;
